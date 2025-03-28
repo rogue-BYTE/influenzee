f13d315acc161e5449aa6cab11ff971ecd10638f
vedant-anand-0 <vedantanandva28@gmail.com>
2025-03-29 02:46:19 +0530

    refactor: Minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import Header from './components/Header.jsx';
-import Footer from "./components/Footer.jsx";
+import Header from './components/Header';
+import Footer from "./components/Footer";
 
-import Landing from './pages/Landing.jsx';
-import AuthPage from "./pages/AuthPage.jsx";
-import Profile from "./pages/Profile.jsx";
-import AboutUs from "./pages/About.jsx";
-import Feed from "./pages/Feed.jsx";
+import Landing from './pages/Landing';
+import AuthPage from "./pages/AuthPage";
+import Profile from "./pages/Profile";
+import AboutUs from "./pages/About";
+import Feed from "./pages/Feed";
 
 function App() {
   return (
